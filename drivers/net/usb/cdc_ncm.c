2e85f2b7ed8e464224e1aa83f2ddc33d8f6f0b25
zlmnlskk <1617133493@qq.com>
2024-12-19 10:19:14 +0800

    Update cdc_ncm.c

--- drivers/net/usb/cdc_ncm.c
+++ drivers/net/usb/cdc_ncm.c
@@ -889,15 +889,15 @@ int cdc_ncm_bind_common(struct usbnet *dev, struct usb_interface *intf, u8 data_
 	 * Some other devices do not work properly with this procedure
 	 * that can be avoided using quirk CDC_MBIM_FLAG_AVOID_ALTSETTING_TOGGLE
 	 */
-	if (!(ctx->drvflags & CDC_MBIM_FLAG_AVOID_ALTSETTING_TOGGLE))
-		usb_set_interface(dev->udev, iface_no, data_altsetting);
-
-	temp = usb_set_interface(dev->udev, iface_no, 0);
-	if (temp) {
-		dev_dbg(&intf->dev, "set interface failed\n");
-		goto error2;
-	}
-
+	// if (!(ctx->drvflags & CDC_MBIM_FLAG_AVOID_ALTSETTING_TOGGLE))
+	//	usb_set_interface(dev->udev, iface_no, data_altsetting);
+	//
+	// temp = usb_set_interface(dev->udev, iface_no, 0);
+	// if (temp) {
+	//	dev_dbg(&intf->dev, "set interface failed\n");
+	//	goto error2;
+	// }
+	//
 	/* initialize basic device settings */
 	if (cdc_ncm_init(dev))
 		goto error2;
@@ -918,9 +918,12 @@ int cdc_ncm_bind_common(struct usbnet *dev, struct usb_interface *intf, u8 data_
 
 	cdc_ncm_find_endpoints(dev, ctx->data);
 	cdc_ncm_find_endpoints(dev, ctx->control);
-	if (!dev->in || !dev->out || !dev->status) {
-		dev_dbg(&intf->dev, "failed to collect endpoints\n");
+	if (!dev->in || !dev->out) {	
+		dev_info(&intf->dev, "failed to collect endpoints\n");
 		goto error2;
+	}
+	if (!dev->status) {
+		dev_info(&intf->dev, "failed to get status\n");
 	}
 
 	usb_set_intfdata(ctx->data, dev);
